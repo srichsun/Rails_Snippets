fe166980627bca7e793665d1af85871be2de5699
Evan <srichsun@gmail.com>
2018-07-05 11:57:09 +0800

    ADD - 第二種 - 用UJS方式替換頁面

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,9 +1,14 @@
 class CountriesController < ApplicationController
   def index
-
   end
 
   def get_a_girl
-    
+  end
+
+  def get_a_boy
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :bitches
   root "countries#index"
+  get 'first_way', to: 'countries#first_way'
+  get 'second_way', to: 'countries#second_way'
   post '/get_a_girl', to: 'countries#get_a_girl'
+  get '/get_a_boy', to: 'countries#get_a_boy'
+  post '/get_a_boy', to: 'countries#get_a_boy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
